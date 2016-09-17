efc56f7fd459c9ce92269ccda7529cc67be6e39f
MrRiegel <ichhabads@gmail.com>
2016-09-17 16:51:17 +0200

    better model

--- src/main/java/mrriegel/pipes/tile/TilePipeBase.java
+++ src/main/java/mrriegel/pipes/tile/TilePipeBase.java
@@ -1,10 +1,38 @@
 package mrriegel.pipes.tile;
 
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import mrriegel.limelib.tile.CommonTile;
 
-public abstract class TilePipeBase extends CommonTile{
-	
-	public abstract boolean validConnection(EnumFacing facing);
+public class TilePipeBase extends CommonTile {
+
+	protected Map<EnumFacing, Boolean> valids = Maps.newHashMap();
+
+	{
+		for (EnumFacing f : EnumFacing.VALUES)
+			valids.put(f, true);
+	}
+
+	public boolean validConnection(EnumFacing facing) {
+		return valids.get(facing);
+	};
+
+	@Override
+	public void readFromNBT(NBTTagCompound compound) {
+		for (EnumFacing f : EnumFacing.VALUES)
+			valids.put(f, compound.hasKey(f.toString()) ? compound.getBoolean(f.toString()) : true);
+		super.readFromNBT(compound);
+	}
+
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+		for (EnumFacing f : EnumFacing.VALUES)
+			compound.setBoolean(f.toString(), valids.get(f));
+		return super.writeToNBT(compound);
+	}
 
 }
